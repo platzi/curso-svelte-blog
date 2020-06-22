afa0deb448c7191823a09de2227bc91cea225a2f
Oscar Barajas Tavares <oscar@arepa.dev>
2020-06-22 10:48:02 -0500

    This is not the commit message you are looking for

---
+++ src/routes/contact.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Contact</title>
+</svelte:head>
+
+<div class="Contact">
+  <h1>Contact</h1>
+  <p>Yout contact information</p>
+</div>
---
+++ src/routes/general.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>General</title>
+</svelte:head>
+
+<div class="General">
+  <h1>General</h1>
+</div>
